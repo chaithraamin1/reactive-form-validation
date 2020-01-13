2d6f2cdb29166f0f2cd59f098415b51b5d048a17
chaithraamin1 <chaithra.amin98@gmail.com>
2020-01-13 08:50:04 +0530

    reactive form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{FormBuilder,FormGroup,Validators}from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveform-validation';
+  rForm:any;
+  post:any;
+  description:string='';
+  name:string=''
+  constructor(private fb:FormBuilder){
+    this.rForm=fb.group({
+      'name':['',Validators.required],
+      'description':['',([Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(500)])],
+       'validate':''
+    });
+  }
+  addPost(post)
+  {
+this.description=post.description;
+this.name=post.name;
+
+  }
 }
